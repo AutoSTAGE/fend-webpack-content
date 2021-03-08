959409524b2800469dd7808fa37d095284949931
AutoSTAGE <jm@autostage.de>
2021-03-08 18:47:26 +0100

    loader finished

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,9 @@
 import { handleSubmit } from './js/formHandler'
-// import { checkForName } from './js/nameChecker'
+import { checkForName } from './js/nameChecker'
 
+/* need to use the exports somewhere for Webpack to work */
+console.log(handleSubmit);
+console.log(checkForName);
+
+/* show alert box */
 alert("I exist!");
